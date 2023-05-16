73cf850072a539439f85f408a1ed39f941136b50
dabrio <ruben.dabrio@gmail.com>
2023-05-16 20:46:50 +0200

    domain

--- src/src/dominio/Banco.java
+++ src/src/dominio/Banco.java
@@ -1,4 +1,35 @@
 package dominio;
 
+import java.util.HashMap;
+
 public class Banco {
+    private String nombre;
+    private HashMap<Integer, CuentaBancaria> cuentas;
+
+    public Banco(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void addCuenta(CuentaBancaria c) {
+        cuentas.put(c.getId(), c);
+    }
+
+    public void removeCuenta(String id) {
+        cuentas.remove(id);
+    }
+
+    public HashMap<Integer, CuentaBancaria> getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(HashMap<Integer, CuentaBancaria> cuentas) {
+        this.cuentas = cuentas;
+    }
 }
--- src/src/dominio/CuentaBancaria.java
+++ src/src/dominio/CuentaBancaria.java
@@ -1,4 +1,27 @@
 package dominio;
 
 public class CuentaBancaria {
+    private int id;
+    private Banco bancoAdmin;
+
+    public CuentaBancaria(int id, Banco bancoAdmin) {
+        this.id = id;
+        this.bancoAdmin = bancoAdmin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBancoAdmin(Banco bancoAdmin) {
+        this.bancoAdmin = bancoAdmin;
+    }
+
+    public Banco getBancoAdmin() {
+        return bancoAdmin;
+    }
 }
