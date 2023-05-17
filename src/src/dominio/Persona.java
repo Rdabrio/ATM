24346ab2fb0fc616f5adbe6df36b5b2e97e208bc
dabrio <ruben.dabrio@gmail.com>
2023-05-17 10:08:26 +0200

    domain

--- src/src/dominio/Persona.java
+++ src/src/dominio/Persona.java
@@ -1,4 +1,11 @@
 package dominio;
 
 public class Persona {
+    private String email;
+    private String nombre;
+
+    public Persona(String email, String nombre) {
+        this.email = email;
+        this.nombre = nombre;
+    }
 }
