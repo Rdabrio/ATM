d8765f905bb4f07210dfa78230abed926650803a
dabrio <ruben.dabrio@gmail.com>
2023-07-13 23:14:15 +0200

    interfaz principal + iniciar sesion

--- src/src/Presentacion/vistas/VPrincipal.java
+++ src/src/Presentacion/vistas/VPrincipal.java
@@ -1,19 +1,33 @@
 package Presentacion.vistas;
 
 import Presentacion.controlador.CtrlPresentacion;
+import dominio.excepciones.PasswordIncorrecta;
+import dominio.excepciones.UsuarioNoExiste;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class VPrincipal {
     private CtrlPresentacion ctrlPresentacion;
-    private JFrame frameVista;
+    private JFrame frameVista = new JFrame("Vista Principal");
+    private JTextField usuarioTextField;
     private JPanel panelContenidos;
+    private JPasswordField passwordField;
+    private JButton RegistrarseButton;
+    private JButton AltaCuentaButton;
+    private JButton IniciarSesionButton;
+    private JPanel panelBotones;
+    private JLabel UsuarioLabel;
+    private JLabel passwordLabel;
+    private JLabel spaceLabel;
+    private JLabel spaceLabel2;
+    private JLabel spaceLabel3;
+    private JLabel spaceLabel4;
+    private JLabel spaceLabel5;
 
     public VPrincipal(CtrlPresentacion ctrlPresentacion) {
         this.ctrlPresentacion = ctrlPresentacion;
-        frameVista = new JFrame("Vista Principal");
-        panelContenidos = new JPanel();
     }
 
     public void inicializa() {
@@ -29,5 +43,31 @@ public class VPrincipal {
     public void inicializarComponentes() {
         frameVista.addWindowListener(ctrlPresentacion.getEventosVPrincipal());
         frameVista.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        RegistrarseButton.setActionCommand("registrarseButton");
+        RegistrarseButton.addActionListener(ctrlPresentacion.getEventosVPrincipal());
+        AltaCuentaButton.setActionCommand("altaCuentaButton");
+        AltaCuentaButton.addActionListener(ctrlPresentacion.getEventosVPrincipal());
+        IniciarSesionButton.setActionCommand("iniciarSesionButton");
+        IniciarSesionButton.addActionListener(ctrlPresentacion.getEventosVPrincipal());
+
+
     }
+
+    public void hacerVisible(boolean b) {
+        this.frameVista.setVisible(b);
+    }
+
+    public void iniciarSesion() {
+        String nombreUsuario = usuarioTextField.getText();
+        String password = Arrays.toString(passwordField.getPassword());
+        try {
+            this.ctrlPresentacion.iniciarSesion(nombreUsuario, password);
+        } catch(PasswordIncorrecta | UsuarioNoExiste e) {
+            usuarioTextField.setText("");
+            passwordField.setText("");
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
 }
